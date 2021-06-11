07b6c701797ce060c54e83bc3417ba0555263f82
afcastaneda223 <af.castaneda223@gmail.com>
2021-06-10 22:34:06 -0300

    add validations to user input for new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
   belongs_to :category
   has_one_attached :image
   acts_as_votable
+  validates :title, length: { minimum: 5, maximum: 100 }
+  validates :body, length: { maximum: 500, minimum: 10 }
+  validates :image, presence: true
 end
